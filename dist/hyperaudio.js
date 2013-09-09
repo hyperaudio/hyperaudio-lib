047fb9761e3ea3689e3834149ba793cf7d2e424a
Happyworm <markp@happyworm.com>
2013-09-09 20:41:25 +0100

    Added the built dist folder.

---
+++ dist/hyperaudio.js
@@ -0,0 +1,503 @@
+/*! hyperaudio 9th September 2013 20:39:30 */
+var ha = (function(window, document) {
+
+var DragDrop = (function (window, document) {
+
+	function DragDrop (handle, droppable, options) {
+		this.options = {
+			init: true,
+			touch: true,
+			mouse: true,
+			timeout: 500,
+			html: ''
+		};
+
+		for ( var i in options ) {
+			this.options[i] = options[i];
+		}
+
+		this.droppable = typeof droppable == 'string' ? document.querySelector(droppable) : droppable;
+
+		// Create the list and the placeholder
+		this.list = this.droppable.querySelector('ul');
+		if ( !this.list ) {
+			this.list = document.createElement('ul');
+			this.droppable.appendChild(this.list);
+		}
+		this.placeholder = document.createElement('li');
+		this.placeholder.className = 'placeholder';
+
+		if ( this.options.init ) {
+			this.handle = typeof handle == 'string' ? document.querySelector(handle) : handle;
+
+			// Are we reordering the list?
+			this.reordering = this.handle.parentNode == this.list;
+
+			if ( this.options.touch ) {
+				this.handle.addEventListener('touchstart', this, false);
+			}
+
+			if ( this.options.mouse ) {
+				this.handle.addEventListener('mousedown', this, false);
+			}
+		}
+	}
+
+	DragDrop.prototype.handleEvent = function (e) {
+		switch (e.type) {
+			case 'mousedown':
+				if ( e.which !== 1 ) {
+					break;
+				}
+			case 'touchstart':
+				this.start(e);
+				break;
+			case 'touchmove':
+			case 'mousemove':
+				this.move(e);
+				break;
+			case 'touchend':
+			case 'mouseup':
+				this.end(e);
+				break;
+		}
+	};
+
+	DragDrop.prototype.start = function (e) {
+		e.preventDefault();
+
+		clearTimeout(this.dragTimeout);
+		this.initiated = false;
+		this.lastTarget = null;
+
+		this.dragTimeout = setTimeout(this.init.bind(this, this.options.html, e), this.options.timeout);
+	};
+
+	DragDrop.prototype.init = function (html, e) {
+		if ( this.options.touch ) {
+			document.addEventListener('touchend', this, false);
+		}
+
+		if ( this.options.mouse ) {
+			document.addEventListener('mouseup', this, false);
+		}
+
+		// Create draggable
+		this.draggable = document.createElement('div');
+		this.draggable.className = 'draggable';
+		this.draggableStyle = this.draggable.style;
+		this.draggableStyle.cssText = 'position:absolute;z-index:1000;pointer-events:none;left:-99999px';
+		this.draggable.innerHTML = html;
+
+		document.body.appendChild(this.draggable);
+
+		this.draggableCenterX = Math.round(this.draggable.offsetWidth / 2);
+		this.draggableCenterY = Math.round(this.draggable.offsetHeight / 2);
+
+		this.position(e);
+
+		if ( this.options.touch ) {
+			document.addEventListener('touchmove', this, false);
+		}
+
+		if ( this.options.mouse ) {
+			document.addEventListener('mousemove', this, false);
+		}
+
+		this.initiated = true;
+
+		// If we are reordering the list, hide the current element
+		if ( this.reordering ) {
+			this.handle.style.display = 'none';
+		}
+
+		this.move(e);
+
+		if ( this.options.onDragStart ) {
+			this.options.onDragStart.call(this);
+		}
+	};
+
+	DragDrop.prototype.position = function (e) {
+		var point = e.changedTouches ? e.changedTouches[0] : e;
+
+		this.draggableStyle.left = point.pageX - this.draggableCenterX + 'px';
+		this.draggableStyle.top = point.pageY - this.draggableCenterY + 'px';
+	};
+
+	DragDrop.prototype.move = function (e) {
+		e.preventDefault();
+
+		var point = e.changedTouches ? e.changedTouches[0] : e;
+		var target = e.touches ? document.elementFromPoint(point.pageX, point.pageY) : point.target;
+
+		this.position(e);
+
+		if ( target == this.lastTarget || target == this.placeholder || target == this.list ) {
+			return;
+		}
+
+		this.lastTarget = target;
+
+		if ( this.list.querySelector('.placeholder') ) {
+			this.list.removeChild(this.placeholder);
+		}
+
+		if ( target == this.droppable ) {
+			this.list.appendChild(this.placeholder);
+			return;
+		}
+
+		if ( target.className == 'item' ) {
+			var items = this.list.querySelectorAll('.item'),
+				i = 0, l = items.length;
+			for ( ; i < l; i++ ) {
+				if ( target == items[i] ) {
+					this.list.insertBefore(this.placeholder, items[i]);
+					break;
+				}
+			}
+		}
+	};
+
+	DragDrop.prototype.end = function (e) {
+		clearTimeout(this.dragTimeout);
+
+		document.removeEventListener('touchend', this, false);
+		document.removeEventListener('mouseup', this, false);
+
+		if ( !this.initiated ) {
+			return;
+		}
+
+		document.removeEventListener('touchmove', this, false);
+		document.removeEventListener('mousemove', this, false);
+
+		var point = e.changedTouches ? e.changedTouches[0] : e;
+		var target = e.touches ? document.elementFromPoint(point.pageX, point.pageY) : point.target;
+
+		var html = this.draggable.innerHTML;
+		this.draggable.parentNode.removeChild(this.draggable);
+		this.draggable = null;
+
+		// if we are reordering, remove the original element
+		if ( this.reordering ) {
+			if ( this.handle._dragInstance ) {
+				this.handle._dragInstance.destroy();
+				this.handle._dragInstance = null;
+			}
+
+			this.handle.parentNode.removeChild(this.handle);
+		}
+
+		// we dropped outside of the draggable area, so exit
+		if ( !this.list.querySelector('.placeholder') ) {
+			return;
+		}
+
+		var el = document.createElement('li');
+		el.className = 'item';
+		el.innerHTML = html;
+
+		this.list.insertBefore(el, this.placeholder);
+		this.placeholder.parentNode.removeChild(this.placeholder);
+
+		if ( this.options.onDrop ) {
+			this.options.onDrop.call(this, el);
+		}
+	};
+
+	DragDrop.prototype.destroy = function () {
+		document.removeEventListener('touchstart', this, false);
+		document.removeEventListener('touchmove', this, false);
+		document.removeEventListener('touchend', this, false);
+
+		document.removeEventListener('mousedown', this, false);
+		document.removeEventListener('mousemove', this, false);
+		document.removeEventListener('mouseup', this, false);
+	}
+
+	return DragDrop;
+})(window, document);
+var WordSelect = (function (window, document) {
+
+	function addTagHelpers (el) {
+		var text = (el.innerText || el.textContent).split(' ');
+
+		el.innerHTML = '<a>' + text.join(' </a><a>') + '</a>';
+	}
+
+	function hasClass (e, c) {
+		if ( !e ) return false;
+
+		var re = new RegExp("(^|\\s)" + c + "(\\s|$)");
+		return re.test(e.className);
+	};
+
+	function addClass (e, c) {
+		if ( hasClass(e, c) ) {
+			return;
+		}
+
+		var newclass = e.className.split(' ');
+		newclass.push(c);
+		e.className = newclass.join(' ');
+	};
+
+	 function removeClass (e, c) {
+		if ( !hasClass(e, c) ) {
+			return;
+		}
+
+		var re = new RegExp("(^|\\s)" + c + "(\\s|$)", 'g');
+		e.className = e.className.replace(re, ' ');
+	};
+
+	function WordSelect (el, options) {
+		this.element = document.querySelector(el);
+
+		this.options = {
+			addHelpers: false,
+			touch: true,
+			mouse: true,
+			threshold: 10
+		};
+
+		for ( var i in options ) {
+			this.options[i] = options[i];
+		}
+
+		if ( this.options.addHelpers ) {
+			addTagHelpers(this.element);
+		}
+
+		this.words = this.element.querySelectorAll('a');
+		this.wordsCount = this.words.length;
+
+		if ( this.options.touch ) {
+			this.element.addEventListener('touchstart', this, false);
+		}
+
+		if ( this.options.mouse ) {
+			this.element.addEventListener('mousedown', this, false);
+		}
+	}
+
+	WordSelect.prototype.handleEvent = function (e) {
+		switch (e.type) {
+			case 'mousedown':
+				if ( e.which !== 1 ) {
+					break;
+				}
+			case 'touchstart':
+				this.start(e);
+				break;
+			case 'touchmove':
+			case 'mousemove':
+				this.move(e);
+				break;
+			case 'touchend':
+			case 'mouseup':
+				this.end(e);
+				break;
+		}
+	};
+
+	WordSelect.prototype.start = function (e) {
+		e.preventDefault();
+
+		var point = e.touches ? e.touches[0] : e;
+
+		this.selectStarted = false;
+		this.startX = e.pageX;
+		this.startY = e.pageY;
+
+		if ( this.options.mouse ) {
+			this.element.addEventListener('mousemove', this, false);
+			this.element.addEventListener('mouseup', this, false);
+		}
+
+		if ( this.options.touch ) {
+			this.element.addEventListener('touchmove', this, false);
+			this.element.addEventListener('touchend', this, false);
+		}
+
+		if ( hasClass(e.target, 'selected') ) {
+			this.dragTimeout = setTimeout(this.dragStart.bind(this, e), 500);
+		}
+	};
+
+	WordSelect.prototype.selectStart = function (e) {
+		var target = e.target,
+			tmp;
+
+		if ( target == this.element ) {
+			return;
+		}
+
+		this.selectStarted = true;
+
+		this.currentWord = target;
+
+		removeClass(this.element.querySelector('.first'), 'first');
+		removeClass(this.element.querySelector('.last'), 'last');
+
+		if ( this.words[this.startPosition] === target ) {
+			tmp = this.startPosition;
+			this.startPosition = this.endPosition;
+			this.endPosition = tmp;
+			return;
+		}
+
+		if ( this.words[this.endPosition] === target ) {
+			return;
+		}
+
+		for ( var i = 0; i < this.wordsCount; i++ ) {
+			if ( this.words[i] == target ) {
+				this.startPosition = i;
+			}
+
+			removeClass(this.words[i], 'selected');
+		}
+
+		this.endPosition = this.startPosition;
+
+		addClass(target, 'selected');
+	};
+
+	WordSelect.prototype.move = function (e) {
+		var point = e.changedTouches ? e.changedTouches[0] : e,
+			target = e.touches ? document.elementFromPoint(point.pageX, point.pageY) : point.target,
+			endPosition;
+
+		if ( Math.abs(point.pageX - this.startX) < this.options.threshold &&
+			Math.abs(point.pageY - this.startY) < this.options.threshold ) {
+			return;
+		}
+
+		clearTimeout(this.dragTimeout);
+
+		if ( !this.selectStarted ) {
+			this.selectStart(e);
+			return;
+		}
+
+		if ( target == this.element || target == this.currentWord ) {
+			return;
+		}
+
+		for ( var i = 0; i < this.wordsCount; i++ ) {
+			if ( this.words[i] == target ) {
+				endPosition = i;
+			}
+
+			if ( ( endPosition === undefined && i >= this.startPosition ) ||
+				( endPosition !== undefined && i <= this.startPosition ) ||
+				endPosition == i ) {
+				addClass(this.words[i], 'selected');
+			} else {
+				removeClass(this.words[i], 'selected');
+			}
+		}
+
+		this.currentWord = target;
+		this.endPosition = endPosition;
+	};
+
+	WordSelect.prototype.end = function (e) {
+		clearTimeout(this.dragTimeout);
+
+		if ( this.options.touch ) {
+			this.element.removeEventListener('touchmove', this, false);
+			this.element.removeEventListener('touchend', this, false);
+		}
+
+		if ( this.options.mouse ) {
+			this.element.removeEventListener('mousemove', this, false);
+			this.element.removeEventListener('mouseup', this, false);
+		}
+
+		if ( !this.selectStarted ) {
+			return;
+		}
+
+		var start = Math.min(this.startPosition, this.endPosition),
+			end = Math.max(this.startPosition, this.endPosition);
+
+		addClass(this.words[start], 'first');
+		addClass(this.words[end], 'last');
+	};
+
+	WordSelect.prototype.clearSelection = function () {
+		removeClass(this.element.querySelector('.first'), 'first');
+		removeClass(this.element.querySelector('.last'), 'last');
+
+		if ( this.options.touch ) {
+			this.element.removeEventListener('touchmove', this, false);
+			this.element.removeEventListener('touchend', this, false);
+		}
+
+		if ( this.options.mouse ) {
+			this.element.removeEventListener('mousemove', this, false);
+			this.element.removeEventListener('mouseup', this, false);
+		}
+
+		var selected = this.element.querySelectorAll('.selected');
+		for ( var i = 0, l = selected.length; i < l; i++ ) {
+			removeClass(selected[i], 'selected');
+		}
+	};
+
+	WordSelect.prototype.getSelection = function () {
+		var selected = this.element.querySelectorAll('.selected');
+		var html = '';
+		for ( var i = 0, l = selected.length; i < l; i++ ) {
+			html += selected[i].innerHTML;
+		}
+
+		return html;
+	};
+
+	WordSelect.prototype.dragStart = function (e) {
+		e.stopPropagation();
+
+		if ( this.options.touch ) {
+			this.element.removeEventListener('touchmove', this, false);
+			this.element.removeEventListener('touchend', this, false);
+		}
+
+		if ( this.options.mouse ) {
+			this.element.removeEventListener('mousemove', this, false);
+			this.element.removeEventListener('mouseup', this, false);
+		}
+
+		var point = e.changedTouches ? e.changedTouches[0] : e;
+
+		if ( this.options.onDragStart ) {
+			this.options.onDragStart.call(this, e);
+		}
+	};
+
+	WordSelect.prototype.destroy = function () {
+		this.element.removeEventListener('touchstart', this, false);
+		this.element.removeEventListener('touchmove', this, false);
+		this.element.removeEventListener('touchend', this, false);
+
+		this.element.removeEventListener('mousedown', this, false);
+		this.element.removeEventListener('mousemove', this, false);
+		this.element.removeEventListener('mouseup', this, false);
+	}
+
+	return WordSelect;
+
+})(window, document);
+var hyperaudio = {};
+
+hyperaudio.utils = {
+	DragDrop: DragDrop,
+	WordSelect: WordSelect
+};
+
+	return hyperaudio;
+}(window, document));
