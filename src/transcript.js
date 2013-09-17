15896e4aa82ea0a540444b30ef15f7fda14f3d32
Happyworm <markp@happyworm.com>
2013-09-17 18:44:24 +0100

    Formalized the module structure.

--- src/transcript.js
+++ src/transcript.js
@@ -7,20 +7,24 @@ var Transcript = (function($) {
 	var DEBUG = true;
 
 	function Transcript(options) {
+		var self = this;
 
-		this.options = {
+		this.options = $.extend({
 			target: '#transcript', // The selector of element where the transcript is written to.
 			src: '', // The source URL of the transcript.
 			group: 'p', // Element type used to group paragraphs.
 			element: 'a', // Element type used per word.
 			attribute: 'm', // Attribute name that holds the timing information.
-			unit: 0.001 // Milliseconds.
+			unit: 0.001, // Milliseconds.
+			debug: false
+		}, options);
+
+		this.event = {
+			error: 'ha:error'
 		};
 
-		for(var i in options) {
-			if(options.hasOwnProperty(i)) {
-				this.options[i] = options[i];
-			}
+		if(DEBUG) {
+			this._debug();
 		}
 
 		if(options.src) {
@@ -38,23 +42,36 @@ var Transcript = (function($) {
 			if($target.length) {
 				$target.empty().load(this.options.src, function(response, status, xhr) {
 					if(status === 'error') {
-						self.error(xhr.status + ' ' + xhr.statusText);
+						self._error(xhr.status + ' ' + xhr.statusText + ' : ' + self.options.src);
 					} else {
 						// Sweet
 					}
 				});
 			} else {
-				this.error('target not found');
+				this._error('target not found : ' + this.options.target);
 			}
 		},
 		parse: function() {
 			//
 		},
-		save: function() {
-			// Doubt we save transcripts from here.
+
+		// Plan to put these into a common object and then simple inheritence by adding to prototype.
+		_trigger: function(eventType, eventData) {
+			var eventObject = $.extend({options: this.options}, eventData),
+				event = $.Event(eventType, {ha: eventObject});
+			$(this).trigger(event);
 		},
-		error: function(msg) {
-			console.log('Transcript Error: ' + msg);
+		_error: function(msg) {
+			var data = {msg: 'Transcript Error : ' + msg};
+			this._trigger(this.event.error, data);
+		},
+		_debug: function() {
+			var self = this;
+			$.each(this.event, function(eventName, eventType) {
+				$(self).on(eventType, function(event) {
+					console.log(eventType + ' : ' + event.ha.msg);
+				});
+			});
 		}
 	};
 
