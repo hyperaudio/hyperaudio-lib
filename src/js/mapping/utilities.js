4ee5c208ae0c5dd96f9cb46124ff4e6e41d88c8a
Happyworm <markp@happyworm.com>
2013-10-25 02:56:56 +0100

    Corrected titleFX utility mapping name.

--- src/js/mapping/utilities.js
+++ src/js/mapping/utilities.js
@@ -4,6 +4,6 @@ hyperaudio.utility('EditBlock', EditBlock); // Class
 hyperaudio.utility('fadeFX', fadeFX); // Class
 hyperaudio.utility('SideMenu', SideMenu); // Class
 hyperaudio.utility('Tap', Tap); // Class
-hyperaudio.utility('titleFX ', titleFX ); // Class
+hyperaudio.utility('titleFX', titleFX ); // Class
 hyperaudio.utility('WordSelect', WordSelect); // Class
 hyperaudio.utility('xhr', xhr); // fn
