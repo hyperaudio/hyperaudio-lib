458845cb500333e113219fb63cf5bbffc15a8526
Happyworm <markp@happyworm.com>
2013-10-25 03:20:18 +0100

    Corrected Popcorn addition to build. Inc pkg and rebuilt

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
 					'build/<%= pkg.name %>.js': [
 
 						// The Popcorn lib and plugins
-						'/lib/popcorn-1.4.0.js',
+						'lib/popcorn-1.4.0.js',
 						'src/js/popcorn.transcript.js',
 
 						// Top closure wrapper
@@ -76,7 +76,7 @@ module.exports = function(grunt) {
 				files: {
 					src: [
 						'Gruntfile.js',
-						'/lib/*.js',
+						// 'lib/*.js',
 						'src/js/*.js'
 					]
 				}
