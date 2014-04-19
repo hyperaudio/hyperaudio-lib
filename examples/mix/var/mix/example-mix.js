211406df7feea1f986e5c4514b5a5fed001b168d
Mark Panaghiston <markp@happyworm.com>
2014-04-19 19:12:37 +0100

    Added in Stage mix example for var input

---
+++ examples/mix/var/mix/example-mix.js
@@ -0,0 +1,124 @@
+var mixContentVar = '' +
+'<article>' +
+	'<section data-effect="title" class="item effect">' +
+		'<form onsubmit="return false">' +
+			'<label>Title: <span class="value">1.5</span>s</label>' +
+			'<div class="effect-checkboxes">' +
+				'<label for="effect-fullscreen">Full Screen:</label>' +
+				'<input disabled type="checkbox" checked="true" id="effect-fullscreen">' +
+			'</div>' +
+			'<input disabled type="text" value="The National Swingers Association wants you!" id="effect-title">' +
+			'<input disabled type="range" step="0.1" max="5" min="0.5" value="1.5" id="effect-duration">' +
+		'</form>' +
+	'</section>' +
+	'<section data-unit="0.001" data-yt="http://www.youtube.com/watch?v=etaCHoeJXCI" class="item">' +
+		'<p>' +
+			'<a class="" data-m="3354">nobody </a>' +
+			'<a class="" data-m="3600">is </a>' +
+			'<a class="" data-m="4000">listening </a>' +
+			'<a class="" data-m="4500">to </a>' +
+			'<a class="" data-m="4700">your </a>' +
+			'<a class="" data-m="4900">telephone </a>' +
+			'<a class="" data-m="5200">calls </a>' +
+		'</p>' +
+	'</section>' +
+	'<section data-effect="fade" class="item effect">' +
+		'<form onsubmit="return false">' +
+			'<label>Fade Effect: <span class="value">0.5</span>s</label>' +
+			'<input disabled type="range" step="0.1" max="5" min="0.5" value="0.5" id="effect-duration">' +
+		'</form>' +
+	'</section>' +
+/*
+	<section data-unit="0.001" data-yt="http://www.youtube.com/watch?v=Jkb5FKlETqY" class="item">
+		<p>
+			<a class="" data-m="11931">I </a>
+			<a class="" data-m="12339">responded </a>
+			<a class="" data-m="12746">in </a>
+			<a class="" data-m="12941">what </a>
+			<a class="" data-m="13136">I </a>
+			<a class="" data-m="13332">thought </a>
+			<a class="" data-m="13527">was </a>
+			<a class="" data-m="13722">the </a>
+			<a class="" data-m="14561">most </a>
+			<a class="" data-m="15001">truthful </a>
+			<a class="" data-m="15640">or </a>
+			<a class="" data-m="16056">least </a>
+			<a class="" data-m="16472">untruthful </a>
+			<a class="" data-m="16887">manner </a>
+		</p>
+	</section>
+	<section data-effect="trim" class="item effect">
+		<form onsubmit="return false">
+			<label>Trim: <span class="value">0.2</span>s</label>
+			<input disabled type="range" step="0.1" max="5" min="0" value="0.2" id="effect-duration">
+		</form>
+	</section>
+	<section data-unit="0.001" data-yt="http://www.youtube.com/watch?v=etaCHoeJXCI" data-id="tcT9CcylTOWkqwAtuNaMKg" class="item">
+		<p>
+			<a class="" data-m="25200">they </a>
+			<a class="" data-m="25400">are </a>
+			<a class="" data-m="25600">not </a>
+			<a class="" data-m="25800">looking </a>
+			<a class="" data-m="26200">at </a>
+			<a class="" data-m="26800">people&apos;s </a>
+			<a class="" data-m="27202">names </a>
+		</p>
+	</section>
+	<section data-ogg="http://hyperaud.io/audio/bgm/06.ColorPlus-Ashore.ogg" data-mp3="http://hyperaud.io/audio/bgm/06.ColorPlus-Ashore.mp3" data-effect="bgm" class="item effect">
+		<form>
+			<div>
+				<span class="icon-music">Ashore, Color Plus</span></div>
+				<label>Delay: <span class="value">0</span>s</label>
+				<input disabled type="range" step="0.5" max="30" min="0" value="0" id="effect-delay">
+				<label>Start At: <span class="value">11</span>s</label>
+				<input disabled type="range" step="0.5" max="30" min="0" value="11" id="effect-start">
+				<label>Duration: <span class="value">3</span>s</label>
+				<input disabled type="range" step="0.5" max="120" min="0" value="3" id="effect-duration">
+				<label>Volume: <span class="value">60</span>%</label>
+				<input disabled type="range" step="5" max="100" min="10" value="60" id="effect-volume">
+			</form>
+		</section>
+		<section data-effect="title" class="item effect">
+			<form onsubmit="return false">
+				<label>Title: <span class="value">1</span>s</label>
+				<div class="effect-checkboxes">
+					<label for="effect-fullscreen">Full Screen:</label>
+					<input disabled type="checkbox" id="effect-fullscreen">
+				</div>
+				<input disabled type="text" value="Yes or No?" id="effect-title">
+				<input disabled type="range" step="0.1" max="5" min="0.5" value="1" id="effect-duration">
+			</form>
+		</section>
+		<section data-unit="0.001" data-yt="http://www.youtube.com/watch?v=Jkb5FKlETqY" class="item">
+			<p>
+				<a class="" data-m="9452">a </a>
+				<a class="" data-m="9700">simple </a>
+				<a class="" data-m="9900">yes </a>
+				<a class="" data-m="10000">or </a>
+				<a class="" data-m="10100">no </a>
+			</p>
+		</section>
+		<section data-unit="0.001" data-yt="http://www.youtube.com/watch?v=etaCHoeJXCI" class="item">
+			<p>
+				<a class="" data-m="7200">that&apos;s </a>
+				<a class="" data-m="8925">not </a>
+				<a class="" data-m="9358">what </a>
+				<a class="" data-m="9700">this </a>
+				<a class="" data-m="9900">program&apos;s </a>
+				<a class="" data-m="10100">about. </a>
+			</p>
+		</section>
+		<section data-effect="trim" class="item effect">
+			<form onsubmit="return false">
+				<label>Trim: <span class="value">0.6</span>s</label>
+				<input disabled type="range" step="0.1" max="5" min="0" value="0.6" id="effect-duration">
+			</form>
+		</section>
+		<section data-effect="fade" class="item effect">
+			<form onsubmit="return false">
+				<label>Fade Effect: <span class="value">1</span>s</label>
+				<input disabled type="range" step="0.1" max="5" min="0.5" value="1" id="effect-duration">
+			</form>
+		</section>
+*/
+	'</article>';
